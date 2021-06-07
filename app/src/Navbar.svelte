96eace16b56c4a2408a156c70dbd232ded0abb44
gbl08ma <gbl08ma@gmail.com>
2021-06-07 01:30:22 +0100

    Quick navbar fixes for mobile

--- app/src/Navbar.svelte
+++ app/src/Navbar.svelte
@@ -22,10 +22,20 @@
         lastReward = reward;
         hideRewardTimeout = setTimeout(() => (lastReward = ""), 7000);
     });
+
+    async function copyAddress(address: string) {
+        try {
+            await navigator.clipboard.writeText(address);
+        } catch (err) {
+            console.error("Failed to copy!", err);
+        }
+    }
 </script>
 
 <nav
-    class="top-0 fixed z-50 h-16 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-white shadow"
+    class="top-0 fixed z-50 {navbarOpen
+        ? 'h-auto'
+        : 'h-16'} w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-white shadow"
 >
     <div class="container max-w-none w-full px-4 mx-auto flex flex-wrap items-center justify-between">
         <div class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
@@ -34,7 +44,7 @@
                 class="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 whitespace-nowrap uppercase"
                 href="/"
             >
-                <img src="/assets/brand/logo.svg" alt="JungleTV" class="h-11 -mb-2">
+                <img src="/assets/brand/logo.svg" alt="JungleTV" class="h-11 -mb-2" />
             </a>
             <button
                 class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
@@ -44,16 +54,33 @@
                 <i class="fas fa-bars" />
             </button>
         </div>
-        <div class="lg:flex flex-grow items-center {navbarOpen ? 'block' : 'hidden'}" id="example-navbar">
+        <div class="lg:flex flex-grow items-center {navbarOpen ? 'block mt-4' : 'hidden'}">
             <ul class="flex flex-col lg:flex-row list-none mr-auto">
                 <li class="flex items-center">
                     {#if rAddress !== ""}
-                        <span class="text-xs text-gray-500">
-                            Rewarding <img src="https://monkey.banano.cc/api/v1/monkey/{rAddress}" alt="MonKey for the address" class="inline h-9 -mt-5 -mb-4 -ml-1 -mr-1"> <span class="font-mono">{rAddress}</span>
+                        <span class="text-xs text-gray-500 mt-2 mb-4 lg:mt-0 lg:mb-0">
+                            Rewarding <img
+                                src="https://monkey.banano.cc/api/v1/monkey/{rAddress}"
+                                alt="MonKey for the address"
+                                title="Click to copy: {rAddress}"
+                                class="inline h-9 -mt-5 -mb-4 -ml-1 -mr-1 cursor-pointer"
+                                on:click={() => copyAddress(rAddress)}
+                            />
+                            <span
+                                class="font-mono cursor-pointer"
+                                title="Click to copy: {rAddress}"
+                                on:click={() => copyAddress(rAddress)}
+                            >
+                                {rAddress.substr(0, 16)}
+                            </span>
                         </span>
                     {/if}
                     {#if lastReward !== ""}
-                        <span class="text-sm text-gray-700 bg-yellow-200 ml-5 p-1 rounded" in:fly={{ x: 200, duration: 1000 }} out:fade>
+                        <span
+                            class="text-sm text-gray-700 bg-yellow-200 ml-5 p-1 rounded"
+                            in:fly={{ x: 200, duration: 1000 }}
+                            out:fade
+                        >
                             Received <span class="font-bold">{apiClient.formatBANPrice(lastReward)} BAN</span>!
                         </span>
                     {/if}
@@ -70,53 +97,31 @@
                 </li>
             </ul>
             <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
-                <!-- <li class="flex items-center">
+                <li class="flex items-center">
                     <a
-                        class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
-                        href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdemos.creative-tim.com%2Fnotus-svelte%2F%23%2F"
-                        target="_blank"
+                        class="text-gray-700 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg hover:bg-yellow-200 outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+                        use:link
+                        href="/about"
                     >
-                        <i class="text-blueGray-400 fab fa-facebook text-lg leading-lg" />
-                        <span class="lg:hidden inline-block ml-2">Share</span>
+                        <i class="fas fa-info text-lg leading-lg mr-2" />
+                        What is this?
                     </a>
                 </li>
 
                 <li class="flex items-center">
                     <a
-                        class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
-                        href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fdemos.creative-tim.com%2Fnotus-svelte%2F&text=Start%20your%20development%20with%20a%20Free%20Tailwind%20CSS%20and%20Svelte%20UI%20Kit%20and%20Admin.%20Let%20Notus%20Svelte%20amaze%20you%20with%20its%20cool%20features%20and%20build%20tools%20and%20get%20your%20project%20to%20a%20whole%20new%20level."
-                        target="_blank"
-                    >
-                        <i class="text-blueGray-400 fab fa-twitter text-lg leading-lg" />
-                        <span class="lg:hidden inline-block ml-2">Tweet</span>
-                    </a>
-                </li>
-
-                <li class="flex items-center">
-                    <a
-                        class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
-                        href="https://github.com/creativetimofficial/notus-svelte?ref=ns-index-navbar"
-                        target="_blank"
-                    >
-                        <i class="text-blueGray-400 fab fa-github text-lg leading-lg" />
-                        <span class="lg:hidden inline-block ml-2">Star</span>
-                    </a>
-                </li> -->
-
-                <li class="flex items-center">
-                    <a
-                        class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
+                        class="text-purple-700 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg hover:bg-yellow-200 outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
                         use:link
                         href="/rewards/address"
                     >
-                        <i class="text-blueGray-400 fas fa-coins text-lg leading-lg mr-2" />
+                        <i class="fas fa-coins text-lg leading-lg mr-2" />
                         Earn rewards
                     </a>
                 </li>
 
                 <li class="flex items-center">
                     <a
-                        class="bg-yellow-400 text-white active:bg-red-500 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+                        class="bg-yellow-400 text-white text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg hover:bg-yellow-500 outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
                         use:link
                         href="/enqueue"
                     >
