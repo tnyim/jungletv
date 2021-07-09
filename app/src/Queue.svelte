d8f6487eb869bf91b1ea7526b8002a5ee239a8de
sebrock <156824+sebrock@users.noreply.github.com>
2021-07-09 21:49:07 +0200

    Requested -> Enqueued
    issue #22

--- app/src/Queue.svelte
+++ app/src/Queue.svelte
@@ -86,7 +86,7 @@
                 </p>
                 <p class="text-xs">
                     {#if entry.hasRequestedBy() && entry.getRequestedBy().getAddress() != ""}
-                        Requested by <img
+                        Enqueued by <img
                             src="https://monkey.banano.cc/api/v1/monkey/{entry.getRequestedBy().getAddress()}"
                             alt={entry.getRequestedBy().getAddress()}
                             title="Click to copy: {entry.getRequestedBy().getAddress()}"
