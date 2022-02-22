c9398a6d9f836fb88030b5a8590ed036df3c9c6d
gbl08ma <gbl08ma@gmail.com>
2022-02-22 00:18:55 +0000

    Use navigator for returning home on NotFound page

--- app/src/NotFound.svelte
+++ app/src/NotFound.svelte
@@ -1,6 +1,10 @@
+<script lang="ts">
+  import { link } from "svelte-navigator";
+</script>
+
 <div>
   <center>
     <h1 style="text-shadow: 3px 3px 3px lightgray; font-size: 12em;" class="text-black dark:text-white">404</h1>
-    <span>This page doesn't exist. Check the URL, or return <a href="/">home</a>.</span>
+    <span>This page doesn't exist. Check the URL, or return <a href="/" use:link>home</a>.</span>
   </center>
 </div>
