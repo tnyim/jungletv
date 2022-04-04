1fe7f48db4533effa0710216d290898a190d40ac
gbl08ma <gbl08ma@gmail.com>
2022-04-05 00:52:52 +0100

    Add close button to media picker

--- app/src/ChatMediaPicker.svelte
+++ app/src/ChatMediaPicker.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { createEventDispatcher } from "svelte";
+    import { slide } from "svelte/transition";
     import ChatEmojiPicker from "./ChatEmojiPicker.svelte";
     import ChatGifPicker from "./ChatGifPicker.svelte";
     import ChatSettings from "./ChatSettings.svelte";
@@ -22,18 +23,21 @@
     }
 </script>
 
-<div on:keydown={onKeyDown}>
+<div on:keydown={onKeyDown} transition:slide={{ duration: 200 }}>
     <div class="flex flex-row flex-wrap px-1 border-l border-r border-gray-300 dark:border-gray-700">
         <SidebarTabButton selected={selectedTab == "emoji"} on:click={() => (selectedTab = "emoji")}>
             Emoji
         </SidebarTabButton>
         <SidebarTabButton selected={selectedTab == "gifs"} on:click={() => (selectedTab = "gifs")}
-            >GIFs</SidebarTabButton
-        >
+            >GIFs
+        </SidebarTabButton>
         <div class="flex-grow" />
         <SidebarTabButton selected={selectedTab == "settings"} on:click={() => (selectedTab = "settings")}
             >Settings</SidebarTabButton
         >
+        <SidebarTabButton selected={false} on:click={() => dispatch("closePicker")} extraClasses="w-8 text-center">
+            <i class="fas fa-times" />
+        </SidebarTabButton>
     </div>
     <div class="h-72">
         {#if selectedTab == "emoji"}
--- app/src/SidebarTabButton.svelte
+++ app/src/SidebarTabButton.svelte
@@ -3,6 +3,7 @@
 
     export let selected = false;
     export let bgClasses = "hover:bg-gray-200 dark:hover:bg-gray-800";
+    export let extraClasses = "";
 
     const dispatch = createEventDispatcher();
 
@@ -18,7 +19,7 @@
     on:click
     on:mousedown
     on:keydown={keyDown}
-    class="{selected
+    class="{extraClasses} {selected
         ? 'font-semibold'
         : 'text-purple-700 dark:text-purple-500'} focus-visible:ring-1 focus-visible:ring-purple-500 focus-visible:ring-inset text-lg px-1.5 py-1 rounded hover:shadow-lg {bgClasses} outline-none focus:outline-none ease-linear transition-all duration-150 cursor-pointer whitespace-nowrap"
 >
