5eb588d9a270a18d82e4a27c7b81e484b5c5475b
gbl08ma <gbl08ma@gmail.com>
2023-03-10 19:38:09 +0000

    Fix serialization of large integers coming from the database into the JS runtimes

--- server/components/apprunner/modules/db/module.go
+++ server/components/apprunner/modules/db/module.go
@@ -2,6 +2,8 @@ package db
 
 import (
 	"context"
+	"fmt"
+	"math"
 	"time"
 
 	"github.com/dop251/goja"
@@ -91,8 +93,16 @@ func (m *dbModule) query(call goja.FunctionCall) goja.Value {
 			// we can modify the result in place without any problems
 			for _, row := range result {
 				for k := range row {
-					if t, ok := row[k].(time.Time); ok {
+					switch t := row[k].(type) {
+					case time.Time:
 						row[k] = gojautil.ToJSDate(runtime, t)
+					case int64:
+						// safe JS integers range from -(2^53-1) to 2^53-1 inclusive
+						// we only pass ints that fit in 32 bits as numbers, to allow some margin for math on them
+						// wider integers get converted to string (in our DB, these usually correspond to snowflake-like IDs)
+						if t < math.MinInt32 || t > math.MaxInt32 {
+							row[k] = fmt.Sprint(t)
+						}
 					}
 				}
 			}
