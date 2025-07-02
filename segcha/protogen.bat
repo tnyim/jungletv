36a9bb708eac1f85ef789e4d4b9047e8dc5f0338
gbl08ma <gbl08ma@gmail.com>
2025-07-02 19:40:09 +0100

    Fix missing vtprotobuf helpers

--- segcha/protogen.bat
+++ segcha/protogen.bat
@@ -1,7 +1,11 @@
 protoc ^
+  --plugin=protoc-gen-go-vtproto=%GOPATH%\bin\protoc-gen-go-vtproto.exe ^
   -I .\segchaproto ^
   --go_opt=paths=source_relative ^
   --go_out=.\segchaproto ^
   --go-grpc_opt=paths=source_relative ^
   --go-grpc_out=.\segchaproto ^
+  --go-vtproto_opt=paths=source_relative ^
+  --go-vtproto_out=.\segchaproto ^
+  --go-vtproto_opt=features=marshal+unmarshal+size ^
   .\segchaproto\segcha.proto
